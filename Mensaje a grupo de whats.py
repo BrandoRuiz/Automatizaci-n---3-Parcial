47b60a3b34330c08bb00da3752cf627e7cf66687
Brando Abraham Ruiz Martinez <102242650+BrandoRuiz@users.noreply.github.com>
2024-03-15 19:02:18 -0600

    Mensajes Clase

---
+++ Mensaje a grupo de whats.py
@@ -0,0 +1,7 @@
+import pywhatkit
+
+try:
+    pywhatkit.sendwhatmsg_to_group("HMSd7sLfFPy4ivP0YtAqvE", "Messi God", 18,57)
+    print("Mensaje enviado")
+except:
+    print("Error")                       #HMSd7sLfFPy4ivP0YtAqvE
---
+++ Mensajes.py
@@ -0,0 +1,7 @@
+import pywhatkit
+
+try:
+    pywhatkit.sendwhatmsg("+524448592496", "Hello word", 18,15)
+    print("Mensaje enviado")
+except:
+    print("Error")                       #HMSd7sLfFPy4ivP0YtAqvE
